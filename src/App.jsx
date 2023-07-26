9e32322c237bf8f1ca2038f72b5c0fa31247c3e3
Yab1 <yabilisanu@gmail.com>
2023-07-26 17:23:58 +0300

    Feature: Utilized the useEffect hook to handle theme preferences and imported Header and Hero components for rendering in the project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+// State and context hooks from React.
+import { useEffect } from "react";
+
+// Components for website displaying.
+import Header from "./Components/Header";
+import HeroSection from "./Components/HeroSection";
+
 function App() {
-  return <></>;
+  useEffect(() => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <HeroSection />
+    </>
+  );
 }
 
 export default App;
